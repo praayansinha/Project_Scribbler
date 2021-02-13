c33f8ee7bdba9ef313538a0bc2610df996d17469
Praayan Sinha <praayan.sinha95@gmail.com>
2021-02-14 02:12:36 +0530

    Homepage added

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+function openSignInForm() {
+    document.body.classList.add("showLoginForm");
+}
+
+function closeSignInForm() {
+    document.body.classList.remove("showLoginForm");
+}
+
+function openSignUpForm() {
+    document.body.classList.add("showSignUpForm");
+}
+
+function closeSignUpForm() {
+    document.body.classList.remove("showSignUpForm");
+}
+
+function openSignUp() {
+    document.body.classList.remove("showLoginForm");
+    openSignUpForm();
+}
+
+function createPost() {
+    document.body.classList.add("showCreatePost");
+}
+
+function closeCreatePost() {
+    document.body.classList.remove("showCreatePost");
+}
+
+function getFocus() { 
+    document.getElementById("username").focus(); 
+} 
